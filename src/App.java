55d319a22c63d905de4f3c8973a78a49f0513792
eimarcoo <97185609+eimarcoo@users.noreply.github.com>
2023-12-05 21:35:06 -0300

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,45 @@
+import java.util.List;
+
+import contracts.iAlunoRepository;
+import exceptions.AlunoNaoEncontradoException;
+import exceptions.MatriculaInvalidaException;
+import repositories.ArrayListAlunoRepository;
+import models.Aluno;
+
+
+public class App {
+    public static void main(String[] args) throws AlunoNaoEncontradoException {
+        try {
+            iAlunoRepository repository = new ArrayListAlunoRepository();
+
+            repository.adicionar(new Aluno("Francisco Apolinário da Silva", "2546321542"));
+            repository.adicionar(new Aluno("Laila Fenandes Lima", "4124563851"));
+            repository.adicionar(new Aluno("Carol Francisma Pereira", "88965215548"));
+            repository.adicionar(new Aluno("Rafael Bernardo Marques", "3652154785"));
+            repository.adicionar(new Aluno("João Paulo Cintra", "2545165266"));
+            repository.adicionar(new Aluno("Karol Duarde e Silva", "5221646885"));
+            repository.adicionar(new Aluno("Lion RIbeiro dos Santos", "63255541523"));
+
+            List<Aluno> listaAlunos = repository.listar();
+            System.out.println("Lista de Alunos:");
+            for (Aluno aluno : listaAlunos) {
+                System.out.println("Nome: " + aluno.getNome() + ", Matrícula: " + aluno.getMatricula());
+            }
+
+            Aluno alunoBuscado = repository.buscar("Rafael Bernardo Marques");
+            System.out.println("\nAluno buscado: " + alunoBuscado.getNome() + ", Matrícula: " + alunoBuscado.getMatricula());
+
+            Aluno excluiraluno = repository.buscar("Laila Fenandes Lima");
+            System.out.println("\nAluno apagado: " + excluiraluno.getNome() + ", Matrícula: " + excluiraluno.getMatricula());
+            repository.remover(excluiraluno);
+
+            List<Aluno> alunosRestantes = repository.listar();
+            System.out.println("\nLista de Alunos atualizada:");
+            for (Aluno aluno : alunosRestantes) {
+                System.out.println("Nome: " + aluno.getNome() + ", Matrícula: " + aluno.getMatricula());
+            }
+        } catch (MatriculaInvalidaException e) {
+            e.printStackTrace();
+        }
+    }
+}
